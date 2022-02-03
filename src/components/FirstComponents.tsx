348adb8082b0ed323dc43d77212f5231ff7e2d6f
jadelcruz1 <jardelcruz6@gmail.com>
2022-02-02 23:24:19 -0300

    Components

---
+++ src/components/FirstComponents.tsx
@@ -0,0 +1,17 @@
+// aula 3 - componente
+
+
+import React, { ReactElement } from 'react'
+
+
+
+function FirstComponents (): ReactElement {
+  return (
+    <div>
+        <h1> Meu primeiro compenente em ts! </h1>
+      
+    </div>
+  )
+}
+
+export default  FirstComponents 
