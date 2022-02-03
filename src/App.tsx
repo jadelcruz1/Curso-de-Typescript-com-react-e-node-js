9ab23eb4803e60c3159489baec50643e4fa6d15e
jadelcruz1 <jardelcruz6@gmail.com>
2022-02-02 22:43:59 -0300

    variaveis no react com ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+
+  // - 1  variaveis
+
+  const name: string ="Jardel";
+
+  const age: number = 37;
+
+  const raça: string = "Negra";
+
+   const isStudent: Boolean = true;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> Projeto React TS</h1>
+
+      <p> Nome: {name}</p>
+
+      <p> Idade: {age}</p>
+
+      <p> Raça: {raça}</p>
+
+      {isStudent && (
+        <p> ele é estudante </p>
+      )}
+      
+    
+    </>
+    
   );
 }
 
